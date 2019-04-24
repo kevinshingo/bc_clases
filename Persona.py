18ea6497e3712efff60927ea0b8669d9603933ad
Kevin Shingo <kevinshingo@hotmail.com>
2019-04-24 22:46:08 +0200

    Se agrego Dino.py y se modifico Persona.py creando la clase persona

--- Persona.py
+++ Persona.py
@@ -1,5 +1,12 @@
+# En el archivo persona.py Crear una clase Persona con atributo nombre
+# Despues instanciar un objeto de tipo persona
 
-# Esto es una prueba
-print("Que tal mundo?")
-print("Probando... probando...")
-print("Probando... 2")
+class Persona:
+    nombre = None
+
+    def __init__(self, un_nombre)
+        self.nombre = un_nombre
+        print("Hola me llamo ", self.nombre)
+
+pepito = Persona("Jose")
+
